4a73bdf1c5570fb0600f80e0ed2c45a0b934252d
Hossik <hossik1983@gmail.com>
2020-07-29 12:26:51 +0300

    sounddelet

--- src/components/Cover/Banner.js
+++ src/components/Cover/Banner.js
@@ -102,7 +102,8 @@ export class BannerImage extends Component  {
           ready: false,
           muted:false,
           images: [
-            "https://soundcloud.com/t-c-ramazan-mutlu/k-sa",
+            "https://raw.githubusercontent.com/Hossik/MyPlanet/blob/master/src/components/FelaKuti.mp3"
+            ,
             "https://soundcloud.com/scumgang6ix9ine/yaya-6ix9ine",
             "https://www.youtube.com/watch?v=SBtjfadiyfk&feature=youtu.be"
 
