445c23b05441da2455133876ee1a5706f6d246e4
Hossik <hossik1983@gmail.com>
2020-08-02 23:40:37 +0300

    works

--- src/components/ToolsComponent.js
+++ src/components/ToolsComponent.js
@@ -1,10 +1,27 @@
 import React from 'react'
-import ResponsiveEmbed from 'react-responsive-embed'
+import Image from 'react-bootstrap/Image'
+import {Col, CardImg, CardImgOverlay,
+  CardTitle ,CardBody} from 'reactstrap';
+import { ExternalLink } from 'react-external-link';
 export const Tools = () => {
 return (
+<div className="container row  offset-lg-11 " style={{background:"blue",marginTop:"10vh" ,height:"50vh"}}>
+      
+      
+      <ExternalLink  href="https://hossik.github.io/confusion/" >
+      <Col xs={4} md={2} md={{ offset: 6 }} >
+ 
+        <div id="imgcover">
+        <img className="img" alt="img" src="https://raw.githubusercontent.com/Hossik/MyPlanet/master/public/assets/confusion.png" /></div>
+        <CardImgOverlay>
+          <CardTitle>some</CardTitle>
+        </CardImgOverlay>
+        </Col>
 
-<CardImg width="100%" src={baseUrl + dish.image} alt={confusion} />
+      </ExternalLink>
+     
 
+</div>
 )
 }
 
