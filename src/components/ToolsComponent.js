cec25210a723626ab20474be49300cece74caf96
Hossik <hossik1983@gmail.com>
2020-08-03 18:21:13 +0300

    works3

--- src/components/ToolsComponent.js
+++ src/components/ToolsComponent.js
@@ -7,14 +7,14 @@ return (
       
       
     <Row>
-      <Col xs={4} sm={{ size: 'auto', offset: 1 }} >
+      <Col  sm={{ size: 'auto', offset: 1 }} >
         <div alt="img" className="img"  >
          <div className="textImg"> Website created during a React Course 
           <ExternalLink  href="https://hossik.github.io/confusion/" > <br/><Button id="visit" color="primary" >Visit Website &#62;</Button></ExternalLink></div> </div></Col>
-          <Col sm={{ size: 'auto', offset: 1 }}  >
-          <div alt="imgtwo" className="img" style={{marginRight:"200px"}}  >
-         <div className="textImg"> Website created during a React Course 
-          <ExternalLink  href="https://hossik.github.io/confusion/" > <br/><Button id="visit" color="primary" >Visit Website &#62;</Button></ExternalLink></div> </div>
+          <Col  sm={{ size: 'auto', offset: 1 }}  >
+          <div alt="imgtwo" className="imgtwo"   >
+         <div className="textImgtwo"><ExternalLink  href="https://hossik.github.io/myplanet/" > <Button id="visit" color="warning" >Go to codes &#62;</Button></ExternalLink><br/> Here is codes of current Website 
+          </div> </div>
       </Col>
      </Row>
 
