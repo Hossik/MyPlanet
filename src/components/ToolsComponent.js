f3f84a1b739f41547e7f8e31f3032bac1cffed0c
Hossik <hossik1983@gmail.com>
2020-08-03 17:44:47 +0300

    works2

--- src/components/ToolsComponent.js
+++ src/components/ToolsComponent.js
@@ -1,17 +1,22 @@
 import React from 'react'
-import {Col,Button} from 'reactstrap';
+import {Col,Button,Row} from 'reactstrap';
 import { ExternalLink } from 'react-external-link';
 export const Tools = () => {
 return (
-<div className="container row  offset-lg-11 " style={{marginTop:"10vh" ,height:"50vh"}}>
+<div className="container   offset-lg-11 " style={{backgroundColor:"red" ,marginTop:"10vh" ,marginLeft:"24vw",height:"50vh"}}>
       
       
-      
-      <Col xs={4} md={{ span: 2, offset: 6 }} >
+    <Row>
+      <Col xs={4} sm={{ size: 'auto', offset: 1 }} >
         <div alt="img" className="img"  >
          <div className="textImg"> Website created during a React Course 
-          <ExternalLink  href="https://hossik.github.io/confusion/" > <Button id="visit" color="primary" >Visit Website &#62;</Button></ExternalLink></div> </div>
+          <ExternalLink  href="https://hossik.github.io/confusion/" > <br/><Button id="visit" color="primary" >Visit Website &#62;</Button></ExternalLink></div> </div></Col>
+          <Col sm={{ size: 'auto', offset: 1 }}  >
+          <div alt="imgtwo" className="img" style={{marginRight:"200px"}}  >
+         <div className="textImg"> Website created during a React Course 
+          <ExternalLink  href="https://hossik.github.io/confusion/" > <br/><Button id="visit" color="primary" >Visit Website &#62;</Button></ExternalLink></div> </div>
       </Col>
+     </Row>
 
       
      
