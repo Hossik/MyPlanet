0a9aaaa0a1ceefbe64ba42e10b3ce3b51dbaabea
Hossik <hossik1983@gmail.com>
2020-08-03 16:37:56 +0300

    works1

--- src/components/ToolsComponent.js
+++ src/components/ToolsComponent.js
@@ -1,24 +1,19 @@
 import React from 'react'
-import Image from 'react-bootstrap/Image'
-import {Col, CardImg, CardImgOverlay,
-  CardTitle ,CardBody} from 'reactstrap';
+import {Col,Button} from 'reactstrap';
 import { ExternalLink } from 'react-external-link';
 export const Tools = () => {
 return (
-<div className="container row  offset-lg-11 " style={{background:"blue",marginTop:"10vh" ,height:"50vh"}}>
+<div className="container row  offset-lg-11 " style={{marginTop:"10vh" ,height:"50vh"}}>
       
       
-      <ExternalLink  href="https://hossik.github.io/confusion/" >
-      <Col xs={4} md={2} md={{ offset: 6 }} >
- 
-        <div id="imgcover">
-        <img className="img" alt="img" src="https://raw.githubusercontent.com/Hossik/MyPlanet/master/public/assets/confusion.png" /></div>
-        <CardImgOverlay>
-          <CardTitle>some</CardTitle>
-        </CardImgOverlay>
-        </Col>
+      
+      <Col xs={4} md={{ span: 2, offset: 6 }} >
+        <div alt="img" className="img"  >
+         <div className="textImg"> Website created during a React Course 
+          <ExternalLink  href="https://hossik.github.io/confusion/" > <Button id="visit" color="primary" >Visit Website &#62;</Button></ExternalLink></div> </div>
+      </Col>
 
-      </ExternalLink>
+      
      
 
 </div>
