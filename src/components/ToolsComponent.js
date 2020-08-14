74bb8e09692baf03d9892f62df4ec3d96d37049b
Hossik <hossik1983@gmail.com>
2020-08-14 19:32:34 +0300

    DEPLOY3

--- src/components/ToolsComponent.js
+++ src/components/ToolsComponent.js
@@ -3,27 +3,40 @@ import {Col,Button} from 'reactstrap';
 import { ExternalLink } from 'react-external-link';
 export const Tools = () => {
 return (
-  <div className="container  " id="projects">  
+  <div className="container  " id="projects" >  
     <div className="row">
-      <Col xs={{size: '10'}} sm={{ size: 'auto', offset: 1 }} >
+      <Col xs={{size: '10'}} lg={{ size: 4}} >
         <div alt="img" className="img"  >
          <div className="textImg"> Website created during by React Course 
           <ExternalLink  href="https://hossik.github.io/confusion/">
-             <br/><Button id="visit" color="primary" >Visit Website &#62;
+             <br/><Button  color="primary" >Visit Website &#62;
           </Button>
           </ExternalLink>
           </div> 
         </div>
       </Col>
-      <Col  xs={{size: '10'}} sm={{ size: 'auto', offset: 1 }}  >
+      <Col  xs={{size: '10'}} lg={{ size: 4}}  >
         <div alt="imgtwo" className="imgtwo"   >
           <div className="textImgtwo"><ExternalLink  href="https://github.com/Hossik/myplanet" > 
-          <Button id="visit" color="warning" >Go to codes &#62;
+          <Button  color="warning" >Go to codes &#62;
           </Button>
           </ExternalLink>
           <br/>
            Here are codes of current React Website 
           </div>
+        </div>
+      </Col>
+      <Col  xs={{size: '10'}} lg={{ size: 4}}  >
+        <div alt="imgtree" className="imgtree"   >
+          <div className="textImgtree">
+          
+           Website created for Gemini Truffle Company 
+
+          <br/>
+          <ExternalLink  href="https://hossik.github.io/truffle" > 
+          <Button color="secondary" >Go to Website &#62;
+          </Button>
+          </ExternalLink></div>
         </div>
       </Col>
      </div>
